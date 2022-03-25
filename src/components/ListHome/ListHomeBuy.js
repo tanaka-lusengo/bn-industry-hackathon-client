59d05bf81ab283591d132c1ded36dfcbfc924dc1
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-03-25 14:41:48 +0000

    Merged new styling and buy api data

--- src/components/ListHome/ListHomeBuy.js
+++ src/components/ListHome/ListHomeBuy.js
@@ -3,31 +3,6 @@ import React from "react";
 import "./ListHome.scss";
 import { Button } from "../Button/Button";
 
-<<<<<<< HEAD
-export default function ListHomeBuy({ sellData }) {
-  return (
-    <article className="list-home">
-      <div className="list-home__left-img-container">
-        <img
-          className="list-home__img"
-          src={sellData.image}
-          alt={sellData.title}
-        />
-      </div>
-      <div className="list-home__middle-text-container">
-        <h2 className="list-home__title">{sellData.title}</h2>
-        <h3 className="list-home__text">Category: {sellData.category}</h3>
-        <p className="list-home__text">{sellData.description}</p>
-        <p className="list-home__text">{sellData.quantity} available</p>
-        <p className="list-home__text">{sellData.price}</p>
-      </div>
-      <div className="list-home__button">
-        <Button buttonName="BUY" />
-      </div>
-    </article>
-  );
-}
-=======
 const ListHomeBuy = ({ itemData }) => {
 	const source = itemData.photo;
 	return (
@@ -53,4 +28,3 @@ const ListHomeBuy = ({ itemData }) => {
 };
 
 export default ListHomeBuy;
->>>>>>> 0b1e228f3a58c8f1997e8ac67b3efb9d14678e80
